47ee9b53a4d24676f6c0b4ed7ef1cf0b3044bb08
OlympusDev <reuben.catchpole@gmail.com>
2019-06-23 16:51:58 +1200

    Added gitignore

--- Novus/ContentView.swift
+++ Novus/ContentView.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct ContentView : View {
     var body: some View {
         HStack(spacing: 0){
-        SideView().frame(minWidth: 150, maxWidth: 150, minHeight: 500, maxHeight: .infinity,  alignment: .topLeading).background(Color.white.blur(radius: 10))
-        BodyView().frame(minWidth: 800, maxWidth: .infinity, minHeight: 500, maxHeight: .infinity,  alignment: .top).background(Color.white)
+            SideView().frame(minWidth: 150, maxWidth: 150, minHeight: 500, maxHeight: .infinity,  alignment: .topLeading).background(Color.primary.colorInvert().blur(radius: 10))
+            BodyView().frame(minWidth: 300, maxWidth: .infinity, minHeight: 500, maxHeight: .infinity,  alignment: .top).background(Color.primary.colorInvert())
         }
     }
 }
@@ -20,8 +20,8 @@ struct ContentView : View {
 struct SideView : View {
     var body: some View {
         VStack{
-        Text("Test")
-        }.padding(.init(top: 50, leading: 10, bottom: 10, trailing: 10))
+            Text("Test")
+            }.padding(.init(top: 50, leading: 10, bottom: 10, trailing: 10))
     }
 }
 
@@ -29,7 +29,7 @@ struct BodyView : View {
     var body: some View {
         HStack(alignment: .top){
             VStack(alignment: .leading) {
-                Text("Monday, 22 June").font(.caption).color(.gray)
+                Text("Monday, 22 June").font(.caption).color(.secondary)
                 Text("Today").font(.title).bold()
             }
             
@@ -37,8 +37,9 @@ struct BodyView : View {
             
             VStack(alignment: .trailing) {
                 HStack{
-                    Text("Search")
-                    Text("Profile")
+                    Image("icons8-search").resizable().frame(width: 25, height: 25)
+                    
+                    Circle().foregroundColor(Color.purple).frame(width: 27, height: 27).overlay(Image("89728389ea0a0201f538832f194ecf0f").resizable().clipShape(Circle()).frame(width: 25, height: 25))
                 }
             }
             
@@ -51,7 +52,7 @@ struct BodyView : View {
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().frame(minWidth: 800, maxWidth: .infinity, minHeight: 500, maxHeight: .infinity,  alignment: .topLeading)
     }
 }
 #endif
