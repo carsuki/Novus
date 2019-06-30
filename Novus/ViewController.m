dad0b10159b95e5461e559d38d0959f0a2068525
EvenDeveloper <evflata@gmail.com>
2019-06-30 18:02:05 +0200

    init

---
+++ Novus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Novus
+//
+//  Created by EvenDev on 30/06/2019.
+//  Copyright © 2019 EvenDev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
---
+++ Novus/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Novus
+//
+//  Created by EvenDev on 30/06/2019.
+//  Copyright © 2019 EvenDev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
