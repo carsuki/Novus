7d77fb00061313ea85fb8243a833013937f7e3d3
EvenDeveloper <evflata@gmail.com>
2019-06-30 17:59:12 +0200

    as

--- Novus/Backend/Repositories/Helpers/NVSAddRepo.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NVSAddRepo.swift
-//  Novus
-//
-//  Created by Diego Magdaleno on 6/27/19.
-//  Copyright © 2019 PolarTeam. All rights reserved.
-//
-
-import Foundation
--- Novus/Today.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Today.swift
-//  Novus
-//
-//  Created by Reuben Catchpole on 28/06/19.
-//  Copyright © 2019 PolarTeam. All rights reserved.
-//
-
-import Foundation
