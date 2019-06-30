7e17c806e4d3387fca01d6836140e43c7de2c9b3
EvenDeveloper <evflata@gmail.com>
2019-06-30 18:02:05 +0200

    init

---
+++ Novus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Novus
+//
+//  Created by EvenDev on 30/06/2019.
+//  Copyright © 2019 EvenDev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Novus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Novus
+//
+//  Created by EvenDev on 30/06/2019.
+//  Copyright © 2019 EvenDev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
