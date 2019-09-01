90e91b88937cb4293e90dfc04ca652fc0e0a17cb
Ultra03 <ultramlg1108@gmail.com>
2019-09-01 13:05:38 -0700

    Simple package installation backend (needs testing)

--- Novus/Backend/DPKG-Parsers/NVSPackage.m
+++ Novus/Backend/DPKG-Parsers/NVSPackage.m
@@ -11,4 +11,50 @@
 
 @implementation NVSPackage
 
+@synthesize identifier;
+@synthesize name;
+@synthesize version;
+@synthesize desc;
+@synthesize section;
+@synthesize architecture;
+@synthesize author;
+@synthesize maintainer;
+@synthesize installedSize;
+@synthesize size;
+
+
+- (id)initWithIdentifier:(NSString *)identifier {
+    
+    self = [super init];
+    
+    if(self) {
+        [self setIdentifier:identifier];
+    }
+    
+    return self;
+    
+}
+
+- (int)install {
+    NVSCommandWrapper *cmdWrapper = [NVSCommandWrapper sharedInstance];
+    NSArray *output = [cmdWrapper runAsRoot:[NSString stringWithFormat:@"apt install %@", [self identifier]]];
+    
+    if(output == NULL) {
+        NSLog([NSString stringWithFormat:@"Error getting root while installing package: %@", [self identifier]]);
+        return -1;
+    }
+    
+    NSString *stdout = [output objectAtIndex:0];
+    NSString *stderr = [output objectAtIndex:1];
+    
+    if(![stderr isEqualToString:@""]) {
+        NSLog([NSString stringWithFormat:@"APT error while installing package %@\nstderr:%@", [self identifier], stdout]);
+        return -2;
+    }
+    
+    NSLog([NSString stringWithFormat:@"Installing package %@...", [self identifier]]);
+    NSLog(stdout);
+    return 0;
+}
+
 @end
